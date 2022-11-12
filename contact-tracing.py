8d5169c53999badbc3cd9e5512501e1db3db836b
Carlosfidelrosales <fidelcarlos302@gmail.com>
2022-11-12 12:52:43 +0800

    Implemented the 3 Menus

---
+++ contact-tracing.py
@@ -0,0 +1,17 @@
+def dict():
+    list_menu = input('''
+
+\033[36;3m
+1. Add an Information
+2. Search 
+3. Exit (y/n) \033[0m
+\033[33;1mWhat do you want to do? (1-3) \033[0m\n
+\033[1m>>>\033[0m ''')
+
+
+
+
+dict()
+
+
+
